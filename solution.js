f230634fac41606a73fc228a3f997a0ab489dbf6
Kairos22 <alandiko84@gmail.com>
2022-07-12 15:28:28 +0200

    Exercise3

---
+++ solution.js
@@ -0,0 +1,24 @@
+// 1
+let i = 0;
+while (i < 15) {
+    console.log(i); i++;
+}
+console.log('--------------------------1');
+
+// 2
+let j = 1;
+let sum = 0;
+while (j <= 20) {
+    sum = sum + j;
+    j++;
+    console.log(sum);
+}
+console.log('---------------------------2');
+
+// 3
+let g = 0;
+do  { 
+    console.log('the number is', g); g++;
+
+} while (g < 20);
+console.log('----------------------------3');
